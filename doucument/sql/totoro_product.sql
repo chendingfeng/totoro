1897b6b49706ae54168decc0dc0d2151443a3ffc
mac <chendingfengmail@163.com>
2020-09-22 08:44:20 +0800

    no message

--- doucument/sql/totoro_product.sql
+++ doucument/sql/totoro_product.sql
@@ -10,7 +10,7 @@ use totoro_product;
 --
 
 CREATE TABLE IF NOT EXISTS `ps_product_category` (
-	id                   bigint not null auto_increment,	
+	id                   bigint NOT NULL auto_increment,
 	parent_id            bigint comment '上级分类的编号：0表示一级分类',
 	name                 varchar(64) comment '名称',
 	level                int(1) comment '分类级别：0->1级；1->2级',
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `ps_product_category` (
 -- 创建商品品牌表
 --
 CREATE TABLE IF NOT EXISTS `ps_product_brand` (
-   id                   bigint not null auto_increment,
+   id                   bigint NOT NULL auto_increment,
    name                 varchar(64) comment '名称',
    first_letter         varchar(8) comment '首字母',
    sort                 int comment '排序',
@@ -41,4 +41,23 @@ CREATE TABLE IF NOT EXISTS `ps_product_brand` (
    big_pic              varchar(255) comment '专区大图',
    brand_story          text comment '品牌故事',
    primary key (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 商品表
+--
+CREATE TABLE IF NOT EXISTS `ps_product` (
+    id                      bigint NOT NULL auto_increment,
+    name                    varchar(100) comment '商品名称',
+    cateid                  varchar(64) DEFAULT NULL comment '类别Id',
+    subtitle                varchar(200) DEFAULT NULL comment '商品副标题',
+    mainimage               varchar(500) DEFAULT NULL comment '产品主图,url相对地址',
+    subimages               text comment '图片地址,json格式',
+    detail text             comment '商品详情',
+    price                   decimal(20,2) NOT NULL comment '价格,单位-元保留两位小数',
+    stock                   int(11) NOT NULL comment '库存数量',
+    status                  int(6) DEFAULT '1' comment '商品状态.1-在售 2-下架 3-删除',
+    createtime datetime     DEFAULT NULL comment '创建时间',
+    updatetime datetime     DEFAULT NULL comment '更新时间',
+    primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
